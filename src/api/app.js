1c9503706caa8fcb1d009c6e57f3f60a8c827160
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-11-29 16:19:10 -0300

    adapta logica de index.js para api/server

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const errorMiddleware = require('../../middlewares/error');
 
 const app = express();
+app.use(express.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use('/users', require('../../routers/usersRouter'));
+
+app.use('/login', require('../../routers/loginRouter'));
+
+app.use(errorMiddleware);
 
 module.exports = app;
