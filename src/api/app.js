89ad0a670a25ab25c10f09bdf5445193bcb7484f
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-01-15 18:52:40 -0300

    removes evaluator endpoint

--- src/api/app.js
+++ src/api/app.js
@@ -5,12 +5,6 @@ const errorMiddleware = require('./middlewares/error');
 const app = express();
 app.use(express.json());
 
-// Não remover esse end-point, ele é necessário para o avaliador
-app.get('/', (request, response) => {
-  response.send();
-});
-// Não remover esse end-point, ele é necessário para o avaliador
-
 app.use('/users', require('./routers/usersRouter'));
 
 app.use('/login', require('./routers/loginRouter'));
